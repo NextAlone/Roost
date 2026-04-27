3ca0a1db8572a4971b6e1eea25f30bcb4ea7df70
Next Alone <12210746+NextAlone@users.noreply.github.com>
2026-04-27 21:10:18 +0800

    feat(vcs): VcsWorktreeControllerResolver + SwiftUI environment key

---
+++ Muxy/Services/Vcs/VcsWorktreeControllerResolver.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftUI
+
+struct VcsWorktreeControllerResolver: Sendable {
+    private let resolve: @Sendable (VcsKind) -> any VcsWorktreeController
+
+    init(_ resolve: @escaping @Sendable (VcsKind) -> any VcsWorktreeController) {
+        self.resolve = resolve
+    }
+
+    func controller(_ kind: VcsKind) -> any VcsWorktreeController {
+        resolve(kind)
+    }
+
+    static let `default` = VcsWorktreeControllerResolver { kind in
+        VcsWorktreeControllerFactory.controller(for: kind)
+    }
+}
+
+private struct VcsWorktreeControllerResolverKey: EnvironmentKey {
+    static var defaultValue: VcsWorktreeControllerResolver { .default }
+}
+
+extension EnvironmentValues {
+    var vcsWorktreeControllerResolver: VcsWorktreeControllerResolver {
+        get { self[VcsWorktreeControllerResolverKey.self] }
+        set { self[VcsWorktreeControllerResolverKey.self] = newValue }
+    }
+}
